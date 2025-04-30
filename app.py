0a5d9603f0b99e3b759401b154609772a6a979b2
daffasatrianegara <daffasatria036@gmail.com>
2025-04-30 16:02:08 +0700

    feat: y gt

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 from dotenv import load_dotenv
 import os
+import requests
 
 app = Flask(__name__)
 load_dotenv()
@@ -12,7 +13,15 @@ conf = {
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    newsUrl = f"{conf['API_URL']}/top-headlines?sources=bbc-news&pageSize=9&apikey={conf['API_KEY']}"
+    try:
+        response = requests.get(newsUrl, timeout=10)
+        response.raise_for_status()
+        newsData = response.json()
+        articles = newsData.get('articles', [])
+    except requests.RequestException:
+        articles = []
+    return render_template('home.html', articles=articles)
 
 @app.route('/news')
 def news():
@@ -21,7 +30,7 @@ def news():
     if(search):
         newsData += f"/everything?q={search}&apikey={conf['API_KEY']}"
     else:
-        newsData += f"/everything?q=latest&apikey={conf['API_KEY']}"
+        newsData += f"/everything?q=lat&apikey={conf['API_KEY']}"
     
     return render_template('news.html', search=search, newsData=newsData)
 
